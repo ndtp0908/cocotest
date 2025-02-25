b389280cc173ef2216c336802d7236b61ec515cb
Phuoc <phuocndtce171602@fpt.edu.vn>
2025-02-25 11:29:03 +0700

    Add project files.

---
+++ coco/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace coco.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
